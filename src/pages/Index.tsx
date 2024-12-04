cad52b3f9f862c0f395d2f9c0c0171a1d41d1e99
canutoilberto <canutoilberto@gmail.com>
2024-12-04 10:46:02 -0300

    Button placeholder config

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -90,7 +90,7 @@ export default function Index() {
           onClick={() => setIsAddModalOpen(true)}
           className="w-full sm:w-auto"
         >
-          <Plus className="mr-2 h-4 w-4" /> Adicionar Item
+          <Plus className="h-4 w-4" /> Adicionar Item
         </Button>
         <div className="relative w-full sm:w-auto">
           <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 h-4 w-4" />
